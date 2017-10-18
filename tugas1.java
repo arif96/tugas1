6cd62625a95b1b0e179bbbd5d83dbf095507d41f
arif96 <32530483+arif96@users.noreply.github.com>
2017-10-18 12:52:46 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,27 @@
+public class tugas1 {
+	public static void main (String [] args) {	
+System.out.println ("Cara Memasak Nasi");
+System.out.println ("1. Cuci beras hingga tiga kali");
+System.out.println ("-tuang atau alirkan air ke dalam wadah rice cooker sambil mengaduk beras");
+System.out.println ("-hingga seluruhnya terendam air,Buang kotoran yang berada di permukaan");
+System.out.println ("-lalu tiriskan beras dengan saringan atau tahan dengan tangan anda di mulut wadah rice cooker.");
+System.out.println ("-Kalau anda memakai panci anti lengket untuk masak nasi, cuci beras anda dalam wadah lain, ya");
+System.out.println ("----------------------------------------------------------------------------------------------");
+System.out.println ("2. Perbandingan air dan beras harus tepat ");
+System.out.println ("-satu gelas beras ditambahkan dua gelas air. Pastikan gelasnya memiliki ukuran yang sama");
+System.out.println ("-Tapi kalau anda pakai beras perah, tambahkan airnya sedikit lebih banyak.");
+System.out.println ("----------------------------------------------------------------------------------------------");
+System.out.println ("3. Tambahkan bumbu ekstra agar nasi bercita rasa dan wangi");
+System.out.println ("-anda bisa tambahkan secuput garam atau beberapa tetes minyak");
+System.out.println ("-Bisa pakai minyak kelapa atau minyak zaitun sesuai selera");
+System.out.println ("-Untuk nasi wangi, tambahkan sedikit cuka atau beberapa helai daun pandan");
+System.out.println ("----------------------------------------------------------------------------------------------");
+System.out.println ("4. Masak beras 20-30 menit, setelah itu diaduk");
+System.out.println ("-Kalau anda masak dengan rice cooker");
+System.out.println ("-anda bisa tahu nasi yang matang saat tombol cook berubah jadi warm");
+System.out.println ("-Namun jika pakai panci, masak beras selama 20-30 menit dan pakai api kecil saja");
+System.out.println ("-Setelah kira-kira matang");
+System.out.println ("diamkan selama kurang lebih 5-10 menit agar beras di dasar panci juga ikut matang");
+
+	}
+}
